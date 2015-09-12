1ede90e5096c9378483c58b248aee8a7ba3c4806
John McGlinn <jmcglinn@essen.local>
2015-09-11 22:52:38 -0700

    init

---
+++ tic/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  tic
+//
+//  Created by John McGlinn on 9/11/15.
+//  Copyright (c) 2015 j2b. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
